a33d87eef38749da67ac7432d9339cf21173b59e
Nixstream <63818018+ajxiong@users.noreply.github.com>
2021-12-17 13:25:27 -0800

    updated inode.java

--- Inode.java
+++ Inode.java
@@ -17,21 +17,21 @@ public class Inode {
 	}
 
 	Inode( short iNumber ) { // retrieving inode from disk
-	// design it by yourself.
-    int blockNumber = 1 + iNumber / 16;
-    byte[] data = new byte[512];
-    Syslib.rawread(blockNumber, data);
-    //initiate offset 
-    int offset = (iNumber % 16) * iNodeSize;
+		// design it by yourself.
+    	int blockNumber = 1 + iNumber / 16;
+    	byte[] data = new byte[512];
+    	Syslib.rawread(blockNumber, data);
+    	//initiate offset 
+    	int offset = (iNumber % 16) * iNodeSize;
 
-    length = Syslib.bytes2int(data, offset);
-    count = Syslib.bytes2short(data, offset += 4);
-    flag = Syslib.bytes2short(data, offset += 2);
+    	length = Syslib.bytes2int(data, offset);
+    	count = Syslib.bytes2short(data, offset += 4);
+    	flag = Syslib.bytes2short(data, offset += 2);
     
-    for(int i = 0; i < directSize; i++)
-      direct[i] = Syslib.bytes2short(data, offset += 2);
+    	for(int i = 0; i < directSize; i++)
+      		direct[i] = Syslib.bytes2short(data, offset += 2);
     
-    indirect = Syslib.bytes2short(data, offset);
+    	indirect = Syslib.bytes2short(data, offset);
 	}
 
 	int toDisk( short iNumber ) { // save to disk as the i-th inode
@@ -50,5 +50,46 @@ public class Inode {
 		
 		SysLib.short2bytes(indirect, data, offset += 2);
 		SysLib.rawwrite(blockNumber, data);
+	}
+
+	//sets index for block
+	public boolean setIndexBlock(short indexNumber){
+		//validate contents of direct[]
+		for(int i = 0; i < directSize; i++){
+			if(direct[i] == -1)
+				return false;
+		}
+
+		if(indirect != -1)
+			return false;
+		
+		indirect = indexNumber;
+		byte[] data = new byte[Disk.blockSize];
+
+		for(int i = 0; i < (Disk.blockSize/ 2); i++)
+			SysLib.short2bytes((short) -1, data, i * 2);
+		
+		SysLib.rawwrite(blockNumber, data);
+		return true;
+	}
+
+	//find target block 
+	public int findTargetBlock(int targetNum){
+		//initialize targetBlock
+		int targetBlock = targetNum / Disk.blockSize;
+		//if targetBlock is less than directSize, return direct index of targetBlock
+		if(targetBlock < directSize)
+			return direct[targetBlock];
+		
+		//if indirect is less than 0, return -1
+		if(indirect < 0)
+			return -1;
+		
+		//otherwise, indirect has a block
+		byte[] data = new byte[Disk.blockSize];
+		SysLib.rawread(indirect, data);
+
+		int offset = (targetBlock - directSize) * 2;
+		return SysLib.bytes2short(data, offset);
 	}
 }
